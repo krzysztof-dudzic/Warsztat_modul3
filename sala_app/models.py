f30ea8ea05d307021ff6c1dd39026c133ff41d3f
krzysztof-dudzic <krzysztof.dudzic12345@gmail.com>
2021-04-18 14:22:08 +0200

    Designing view3

--- sala_app/models.py
+++ sala_app/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class Room(models.Model):
-    name_room = models.CharField(max_length=255, unique=True),
+    name = models.CharField(max_length=255, unique=True),
     capacity = models.IntegerField(),
-    projector_is_available = models.BooleanField()
+    projector = models.BooleanField()
--- sala_app/views.py
+++ sala_app/views.py
@@ -1,12 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
-
+from sala_app.models import *
 
 def index(request):
 
     return render(request, 'start_page.html')
-
 
 
 class AddRoom(View):
@@ -17,13 +16,15 @@ class AddRoom(View):
         name = request.POST.get('name')
         capacity = request.POST.get('capacity')
         projector = request.POST.get('capacity')
+        name_a = Room.objects.all()
         if name == '':
             return HttpResponse("Name of room is empty")
         else:
-            if name:
+            if name in name_a:
                 return HttpResponse("This name already exists")
             else:
-                if capacity < 0:
+                capacity_a = int(capacity)
+                if capacity_a < 0:
                     return HttpResponse("This room is not capacity")
                 else:
                     return render(request, template_name="add_room.html",
